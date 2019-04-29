c375807622741945dfaf7d7844c4292f51ab07e5
Jean-Philippe Braun <eon@patapon.info>
2019-04-29 16:59:49 +0200

    utils: remove unused function

--- tungsten_prometheus_exporter/utils.py
+++ tungsten_prometheus_exporter/utils.py
@@ -1,21 +1,6 @@
-import collections
-
-
 def my_import(name):
     components = name.split(".")
     mod = __import__(components[0])
     for comp in components[1:]:
         mod = getattr(mod, comp)
     return mod
-
-
-def dict_merge(dct, dct_merge):
-    for k, v in dct_merge.items():
-        if (
-            k in dct and
-            isinstance(dct[k], dict) and
-            isinstance(dct_merge[k], collections.Mapping)
-        ):
-            dict_merge(dct[k], dct_merge[k])
-        else:
-            dct[k] = dct_merge[k]
